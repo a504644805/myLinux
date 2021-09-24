0be5469979c97936bc1780b9520cd8383874a879
XY <xiangyuan211@gmail.com>
2021-09-23 18:30:53 -0700

    add backup of miniOS

---
+++ signal_test/cmake-build-debug/CMakeFiles/signal_tes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/signal_tes.dir/main.cpp.o"
+  "signal_tes"
+  "signal_tes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/signal_tes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
